d9f84642c5de901ac8b7d569adb2d5d693dd9f98
Jonatan Nevo <thejonatanmr@gmail.com>
2017-12-21 18:28:20 +0200

    Add Implementaions
    Added implementaions of the BaseWeapon, ConsumableItem, PlayableCharacter and Enemy classes

--- Src/Structure.py
+++ Src/Structure.py
@@ -6,7 +6,7 @@ class BaseCharacter:
         self.name = name
         self.level = level
 
-    def attacked(self, attacker):
+    def attacked(self, value):
         raise NotImplementedError
 
     def interacted(self, other):
@@ -46,14 +46,22 @@ class BaseWeapon(BaseItem):
         self.price = random.randint(5, 5 * self.level, 5) + self.calc_rarity_price()
 
     def use(self, user):
-        # TODO return the attack value (no need for user)
-        pass
+        # To Implement - return the attack value (no need for user)
+        raise NotImplementedError
 
     def bought(self, buyer):
         if not self.is_owned:
-            # TODO when player character is implemented reduce the gold based on the self.price of the weapon and checks if playable character has enough gold
-            self.price /= 2
-            self.is_owned = True
+            if isinstance(buyer, PlayableCharacter):
+                if buyer.gold >= self.price:
+                    buyer.reduce_gold(self.price)
+                    self.price /= 2
+                    self.is_owned = True
+                else:
+                    print "Error, Buyer don't have enough gold"
+            else:
+                print "Error, Buyer is not playable character"
+        else:
+            print "Error, self.is_owned is true"
 
 
 class ConsumableItem(BaseItem):
@@ -62,47 +70,71 @@ class ConsumableItem(BaseItem):
         self.price = random.randint(5, 5 * self.level, 5) + (self.calc_rarity_price() / 10)
 
     def use(self, user):
-        # TODO when enemy and character are implemented add the desired effect
-        pass
+        # To implement - adding the desired effect
+        raise NotImplementedError
 
     def bought(self, buyer):
         if not self.is_owned:
-            # TODO when player character is implemented reduce the gold based on the self.price of the weapon
-            self.price /= 2
-            self.is_owned = True
+            if isinstance(buyer, PlayableCharacter):
+                if buyer.gold >= self.price:
+                    buyer.reduce_gold(self.price)
+                    self.price /= 2
+                    self.is_owned = True
+                else:
+                    print "Error, Buyer don't have enough gold"
+            else:
+                print "Error, Buyer is not playable character"
+        else:
+            print "Error, self.is_owned is true"
 
 
 class PlayableCharacter(BaseCharacter):
-    def __init__(self, level, name="Player", items=[], weapon=None, armor=None):
+    def __init__(self, level, name="Player", items=[], gold=0, weapon=None):
         super(PlayableCharacter, self).__init__(name, level)
         self.items = items
         self.weapon = weapon
-        self.armor = armor
-        # TODO add math to calculate the hp based on level
+        self.gold = gold
+        self.hp = 90 + (self.level * 10)
+        self.xp_cap = 100 * self.level
 
     def attack(self, enemy):
-        # TODO check enemy type and call attacked function with self.weapon
-        pass
+        if isinstance(enemy, Enemy):
+            enemy.attacked(self.weapon.use())
+        else:
+            print "Error, enemy is not instance of Enemy class"
 
     def get_xp(self, xp):
-        # TODO check if over cap if do call level up
-        pass
+        self.xp += xp
+        if self.xp >= self.xp_cap:
+            self.level_up()
 
     def level_up(self):
-        # TODO add level and add hp based on the level
-        pass
+        self.hp += 10
+        self.xp -= self.xp_cap
+        self.xp_cap += 100
+        self.level += 1
 
     def use_item(self, item):
         # TODO check if item exists in self.items. is yes call used function of item and remove from self.items
         pass
 
-    def attacked(self, attacker):
-        # TODO remove hp based on attacker attack values
-        pass
+    def attacked(self, value):
+        self.hp -= value
 
     def dead(self):
         # TODO end current run
         pass
+
+    def is_dead(self):
+        if self.hp <= 0:
+            return True
+        return False
+
+    def reduce_gold(self, amount):
+        if self.gold >= amount:
+            self.gold -= amount
+        else:
+            print "Error, playable character dont have enough gold"
 
     def interacted(self, other):
         if isinstance(other, BaseCharacter):
@@ -122,20 +154,26 @@ class Enemy(BaseCharacter):
         # TODO add math to calculate the hp based on level
 
     def attack(self, enemy):
-        # TODO check enemy type and call attacked function with self.weapon
-        pass
+        if isinstance(enemy, BaseCharacter):
+            enemy.attacked(self.weapon.use())
+        else:
+            print "Error, enemy is not instance of BasicCharacter class"
 
     def use_item(self, item):
         # TODO check if item exists in self.items. is yes call used function of item and remove from self.items
         pass
 
-    def attacked(self, attacker):
-        # TODO remove hp based on attacker attack values
-        pass
+    def attacked(self, value):
+        self.hp -= value
 
     def dead(self, other):
-        # TODO if other is instance of playableCharacter give other gold and xp based on level
-        pass
+        if isinstance(other, PlayableCharacter):
+            return [self.level * random.randint(2, 10, 2), self.level * random.randint(1, 5)]
+
+    def is_dead(self):
+        if self.hp <= 0:
+            return True
+        return False
 
     def interacted(self, other):
         if isinstance(other, BaseCharacter):
